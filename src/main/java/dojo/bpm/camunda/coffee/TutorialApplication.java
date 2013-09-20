e9eaa10f44057c9556cfc31fd5d710f9f173bc7d
pat412 <patrick@xplorer.net>
2013-09-20 13:17:44 +0200

    added project files

---
+++ src/main/java/dojo/bpm/camunda/coffee/TutorialApplication.java
@@ -0,0 +1,9 @@
+package dojo.bpm.camunda.coffee;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class TutorialApplication extends ServletProcessApplication {
+
+}
