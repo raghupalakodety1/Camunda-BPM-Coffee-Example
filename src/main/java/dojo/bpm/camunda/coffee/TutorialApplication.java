52693538a30aef9abdd50c97839f04e3ea75cf80
pat412 <patrick@xplorer.net>
2013-09-21 11:33:30 +0200

    removed obsolete class

--- src/main/java/dojo/bpm/camunda/coffee/TutorialApplication.java
+++
@@ -1,9 +0,0 @@
-package dojo.bpm.camunda.coffee;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Coffee Order App")
-public class TutorialApplication extends ServletProcessApplication {
-
-}
